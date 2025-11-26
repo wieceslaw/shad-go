66d3384cd168672c754605972439a37bb0c2f9b4
wieceslaw <vyache-nov@mail.ru>
2025-11-26 23:07:40 +0300

    semi-completed genericsum

--- genericsum/genericsum.go
+++ genericsum/genericsum.go
@@ -2,26 +2,86 @@
 
 package genericsum
 
-func Min(a, b int) int {
-	panic("implement me")
+import (
+	"cmp"
+	"sync"
+)
+
+func Min[T cmp.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
 
-func SortSlice(a []int) {
-	panic("implement me")
+func SortSlice[T cmp.Ordered](a []T) {
+	for i := range a {
+		for j := i; j < len(a); j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
 }
 
-func MapsEqual(a, b map[int]int) bool {
-	panic("implement me")
+func MapsEqual[K comparable, V comparable](a, b map[K]V) bool {
+	for k, v1 := range a {
+		if v2, ok := b[k]; !ok || v2 != v1 {
+			return false
+		}
+	}
+	for k, v1 := range b {
+		if v2, ok := a[k]; !ok || v2 != v1 {
+			return false
+		}
+	}
+	return true
 }
 
-func SliceContains(s []int, v int) bool {
-	panic("implement me")
+func SliceContains[T comparable](s []T, v T) bool {
+	for _, el := range s {
+		if el == v {
+			return true
+		}
+	}
+	return false
 }
 
-func MergeChans(chs ...<-chan int) <-chan int {
-	panic("implement me")
+func MergeChans[T any](chs ...<-chan T) <-chan T {
+	resCh := make(chan T)
+	var wg sync.WaitGroup
+	wg.Add(len(chs))
+	for _, ch := range chs {
+		go func() {
+			for el := range ch {
+				resCh <- el
+			}
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(resCh)
+	}()
+	return resCh
 }
 
-func IsHermitianMatrix(m [][]int) bool {
+type Numeric interface {
+	~int | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~int8 | ~int16 | ~int32 | ~int64 | ~complex128 | ~complex64 | ~float32 | ~float64
+}
+
+func IsHermitianMatrix[T Numeric](m [][]T) bool {
+	if len(m) == 0 {
+		return true
+	}
+	if len(m) != len(m[0]) {
+		return false
+	}
+
+	for i := range m {
+		for j := range m[i] {
+		}
+	}
+	// equals to transposed matrix with all elements conjugated (real part is intact, while imaginary part is inverted)
 	panic("implement me")
 }
--- treeiter/treeiter.go
+++ treeiter/treeiter.go
@@ -1,3 +1,17 @@
 //go:build !solution
 
 package treeiter
+
+type TreeNode[T any] interface {
+	Left() *T
+	Right() *T
+}
+
+func DoInOrder[N TreeNode[N]](node *N, visit func(*N)) {
+	if node == nil {
+		return
+	}
+	DoInOrder((*node).Left(), visit)
+	visit(node)
+	DoInOrder((*node).Right(), visit)
+}
