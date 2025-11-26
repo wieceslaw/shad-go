54f2f2f025f7e632cd72dc061851108f6220514d
wieceslaw <vyache-nov@mail.ru>
2025-11-26 23:42:22 +0300

    completed lrucache

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -2,30 +2,80 @@
 
 package lrucache
 
-type lruCache struct {
-	cap  int
-	dict map[int]int
+import (
+	"container/list"
+)
+
+type Pair struct {
+	key   int
+	value int
 }
 
-func (c *lruCache) Get(key int) (int, bool) {
-	i, ok := c.dict[key]
-	return i, ok
+type LruCache struct {
+	cap   int
+	dict  map[int]*list.Element
+	order *list.List
 }
 
-func (c *lruCache) Set(key, value int) {
-	c.dict[key] = value
+func (c *LruCache) Get(key int) (int, bool) {
+	el, ok := c.dict[key]
+	if !ok {
+		return 0, false
+	}
+
+	c.order.Remove(el)
+	delete(c.dict, key)
+
+	val := el.Value.(Pair).value
+	c.dict[key] = c.order.PushFront(Pair{key, val})
+
+	return val, ok
 }
 
-func (c *lruCache) Range(f func(key, value int) bool) {
+func (c *LruCache) Set(key, value int) {
+	if c.cap == 0 {
+		return
+	}
 
+	if len(c.dict) >= c.cap {
+		el := c.order.Back()
+		key := el.Value.(Pair).key
+		delete(c.dict, key)
+		c.order.Remove(el)
+	}
+
+	el, ok := c.dict[key]
+	if ok {
+		c.order.Remove(el)
+		delete(c.dict, key)
+	}
+
+	c.dict[key] = c.order.PushFront(Pair{key, value})
 }
 
-func (c *lruCache) Clear() {
+func (c *LruCache) Range(f func(key, value int) bool) {
+	l := c.order
+	for e := l.Back(); e != nil; e = e.Prev() {
+		v := e.Value.(Pair)
+		if !f(v.key, v.value) {
+			return
+		}
+	}
+}
+
+func (c *LruCache) Clear() {
+	for c.order.Len() != 0 {
+		c.order.Remove(c.order.Front())
+	}
 	for k := range c.dict {
 		delete(c.dict, k)
 	}
 }
 
 func New(cap int) Cache {
-	return &lruCache{cap, make(map[int]int)}
+	return &LruCache{
+		cap,
+		make(map[int]*list.Element, cap),
+		list.New(),
+	}
 }
